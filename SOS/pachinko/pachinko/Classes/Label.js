e6d750721014e86f1e07d6d622e0a79373e0de12
201920296 <114163954+201920296@users.noreply.github.com>
2022-11-11 05:11:55 +0900

    GG

--- SOS/pachinko/pachinko/Classes/Label.js
+++ SOS/pachinko/pachinko/Classes/Label.js
@@ -1,11 +1,11 @@
 function Label(content, x, y, size, r, g, b){
-	this.content	= content;
-	this.x			= x;
-	this.y 			= y;
-	this.size		= size;
-	this.r 			= r;
-	this.g 			= g;
-	this.b 			= b;
+	this.content = content;
+	this.x = x;
+	this.y = y;
+	this.size = size;
+	this.r = r;
+	this.g = g;
+	this.b = b;
 	
 }
 
